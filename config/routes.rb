86ef6bf3776a097a8c41869a26347e51ad941907
Matthew Blott <m4ttb@pm.me>
2026-02-12 15:21:18 +0000

    saved

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   controller :static_pages do
     get 'about',   action: :about
     get 'home',    action: :home
-    get 'secret', action: :secret
     get 'sign_in', action: :sign_in
     get 'sign_out', action: :sign_out
   end
